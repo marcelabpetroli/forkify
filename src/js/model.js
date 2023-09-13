3b8ba4f9475c6b39f77674c80ef79ac5cc12fa87
Marcela Petroli <marcelabpetroli@gmail.com>
2023-09-13 13:36:29 +0200

    refactor: create helpers and config files

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
@@ -7,12 +9,7 @@ export const state = {
 // Doesn't return anything, only changes the state object
 export const loadRecipe = async function (id) {
   try {
-    const response = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await response.json();
-
-    if (!response.ok) throw new Error(`${data.message} ${response.status}`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -27,6 +24,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} ❌`);
   }
 };
