844f691a763e3b1b9c3d8259e37558d370723a6c
Marcela Petroli <marcelabpetroli@gmail.com>
2023-09-14 10:36:28 +0200

    feat: implement recipe selected

--- src/js/controller.js
+++ src/js/controller.js
@@ -14,6 +14,8 @@ const controlRecipes = async function () {
 
     recipeView.renderSpinner();
 
+    resultsView.update(model.getSearchResultsPage());
+
     await model.loadRecipe(id);
 
     recipeView.render(model.state.recipe);
